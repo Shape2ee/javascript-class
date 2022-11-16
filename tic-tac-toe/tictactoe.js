bcae8cc56ff41c2fbcab3c069b503b77f228986c
Shape Lee <shape1026@gmail.com>
2022-11-16 23:43:25 +0900

    승부확인하기 추가

--- tic-tac-toe/tictactoe.js
+++ tic-tac-toe/tictactoe.js
@@ -8,6 +8,106 @@ const $result = document.createElement("div");
 let turn = "O";
 const rows = [];
 
+// [
+//   [td, td, td]
+//   [td, td, td]
+//   [td, td, td]
+// ]
+
+const checkWinner = (target) => {
+  let rowIndex;
+  let cellIndex;
+  rows.forEach((row, ri) => {
+    row.forEach((cell, ci) => {
+      if (cell === target) {
+        rowIndex = ri;
+        cellIndex = ci;
+      }
+    });
+  });
+
+  // 세 칸 다 채워졌나?
+  let hasWinner = false;
+
+  //  가로줄 검사
+  if (
+    rows[rowIndex][0].textContent === turn &&
+    rows[rowIndex][1].textContent === turn &&
+    rows[rowIndex][2].textContent === turn
+  ) {
+    hasWinner = true;
+  }
+
+  // 세로줄 검사
+  if (
+    rows[0][cellIndex].textContent === turn &&
+    rows[1][cellIndex].textContent === turn &&
+    rows[2][cellIndex].textContent === turn
+  ) {
+    hasWinner = true;
+  }
+
+  // 대각선 검사
+  if (
+    rows[0][0].textContent === turn &&
+    rows[1][1].textContent === turn &&
+    rows[2][2].textContent === turn
+  ) {
+    hasWinner = true;
+  }
+
+  if (
+    rows[0][2].textContent === turn &&
+    rows[1][1].textContent === turn &&
+    rows[2][0].textContent === turn
+  ) {
+    hasWinner = true;
+  }
+
+  return hasWinner;
+};
+
+const onCheckCells = (event) => {
+  // 칸에 글자가 있나?
+  if (event.target.textContent !== "") {
+    console.log("빈칸이 아닙니다");
+    return;
+  }
+  console.log("빈칸입니다.");
+  event.target.textContent = turn;
+
+  // 승부확인
+  if (checkWinner(event.target)) {
+    console.log("승리");
+    $result.textContent = `${turn}님이 승리입니다.`;
+    $table.removeEventListener("click", onCheckCells);
+    return;
+  }
+
+  // 무승부 검사
+  let draw = true;
+  rows.forEach((row) => {
+    row.forEach((cell) => {
+      if (!cell.textContent) {
+        draw = false;
+      }
+    });
+  });
+  if (draw) {
+    $result.textContent = `무승부입니다.`;
+    return;
+  }
+
+  turn = turn === "O" ? "X" : "O";
+  /* 
+  if (turn === "O") {
+    turn = "X";
+  } else if (turn === "X") {
+    turn = "O";
+  }
+  */
+};
+
 // 이차원 배열 이용하기
 for (let i = 0; i < 3; i++) {
   const $tr = document.createElement("tr");
@@ -15,24 +115,12 @@ for (let i = 0; i < 3; i++) {
   for (let j = 0; j < 3; j++) {
     const $td = document.createElement("td");
     cells.push($td);
-    $td.addEventListener("click", (event) => {
-      // 칸에 글자가 있나?
-      if (event.target.textContent) return;
-
-      event.target.textContent = turn;
-      // 승부확인
-      if (turn === "O") {
-        turn = "X";
-      } else if (turn === "X") {
-        turn = "O";
-      }
-    });
     $tr.append($td);
   }
   rows.push(cells);
   $table.append($tr);
 }
-
+$table.addEventListener("click", onCheckCells);
 body.append($table);
 body.append($result);
 
